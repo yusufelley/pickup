349d5e38e3f40c629ef892f1a8dfab01a7174127
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2021-11-07 05:30:46 -0500

    CounterButton component merge

--- web/src/components/Events/EventDate.js
+++ web/src/components/Events/EventDate.js
@@ -1,15 +1,12 @@
 import "./EventDate.css";
 
 const EventDate = (props) => {
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const year = props.date.getFullYear();
+  const name = "Basketball"
+
 
   return (
     <div className="expense-date">
-      <div className="expense-date__day">{day}</div>
-      <div className="expense-date__month">{month}</div>
-      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__name">{name}</div>
     </div>
   );
 };
--- web/src/components/Events/EventItem.js
+++ web/src/components/Events/EventItem.js
@@ -1,18 +1,29 @@
 import EventDate from "./EventDate";
 import Card from "../UI/Card";
 import "./EventItem.css";
+import CounterButton from "../CounterButton";
+import { useState } from 'react'
 
 const EventItem = (props) => {
+
+  const[hereToggled, setHereToggled] = useState(false)
+
   return (
-    <li>
-      <Card className="expense-item">
-        <EventDate date={props.date} />
-        <div className="expense-item__description">
-          <h2>{props.title}</h2>
-          <div className="expense-item__price">${props.amount}</div>
-        </div>
-      </Card>
-    </li>
+    <Card className="expense-item">
+    <EventDate date={props.date} />
+    <div className="location"> locationName </div>
+    <div className="eventProperties">
+      <div className="startAndDuration">
+        <p> Started at ____</p>
+        <p> Playing for ____</p>
+      </div> 
+      <div className="numsHereComing"> 
+        <CounterButton text = {"Here"} hereToggled={hereToggled} setHereToggled={setHereToggled}/>
+        <CounterButton text = {"Coming"} hereToggled={hereToggled} setHereToggled={setHereToggled}/>
+      </div>
+    </div>
+        
+    </Card>
   );
 };
 
