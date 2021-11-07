5e1799caad063ba33fe786a05f08124c67ff6b82
Yusuf Elley <yelley2001@gmail.com>
2021-11-07 08:03:32 -0500

    remove ref error

--- web/src/components/Header/Settings.js
+++ web/src/components/Header/Settings.js
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
 import "./Settings.css";
 
-
 const Settings = (props) => {
-
   const [userInput, setUserInput] = useState({
     Email: "",
     Password: "",
   });
-
-
 
   const emailAddedHandler = (setting) => {
     setUserInput((prevState) => {
@@ -25,50 +21,48 @@ const Settings = (props) => {
     });
   };
 
-  const prefHandler = (event) => {
-      event.target.value
-  }
-
-
-  
-return (
+  return (
     <div className="Settings">
-        <span>
+      <span>
         <div className="closeButton">
-            <div onClick = {() => props.setShowSettings(false)}>Close</div>
+          <div onClick={() => props.setShowSettings(false)}>Close</div>
         </div>
-        </span>
-        <div className="Account">
-            <h2>Account</h2>
-            <div class="emai;"></div>
-            <label for="email">Email</label>
-            <input type="text" id="email" placeholder="Enter email"></input>
-            <div class="password"></div>
-            <label for="password">Password</label>
-            <input type="password" id="password" placeholder="Enter password"></input>
-        </div>
-        <div className="Preferences">
-            <h2>Preferences</h2>
-            <select onChange={prefHandler}>
-            <option value="none" defaultValue>
-                   All Sports
-                </option>
-                <option value="Basketball">Basketball</option>
-                <option value="Soccer">Soccer</option>
-                <option value="Football">Football</option>
-                <option value="Volleyball">Volleyball</option>
-                <option value="Quidditch">Quidditch</option>
-                <option value="Rugby">Rugby</option>
-            </select>
-        </div>
+      </span>
+      <div className="Account">
+        <h2>Account</h2>
+        <div class="emai;"></div>
+        <label for="email">Email</label>
+        <input type="text" id="email" placeholder="Enter email"></input>
+        <div class="password"></div>
+        <label for="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter password"
+        ></input>
+      </div>
+      <div className="Preferences">
+        <h2>Preferences</h2>
+        <select>
+          <option value="none" defaultValue>
+            All Sports
+          </option>
+          <option value="Basketball">Basketball</option>
+          <option value="Soccer">Soccer</option>
+          <option value="Football">Football</option>
+          <option value="Volleyball">Volleyball</option>
+          <option value="Quidditch">Quidditch</option>
+          <option value="Rugby">Rugby</option>
+        </select>
+      </div>
 
-        <span>
+      <span>
         <div className="Submit">
-            <div onClick = {() => props.setShowSettings(false)}>Submit</div>
+          <div onClick={() => props.setShowSettings(false)}>Submit</div>
         </div>
-        </span>    
+      </span>
     </div>
   );
 };
 
-export default Settings
+export default Settings;
