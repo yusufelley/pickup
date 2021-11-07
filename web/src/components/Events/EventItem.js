5c41a06f173eb5e72de3088a57306a593fe04ccc
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2021-11-07 08:11:40 -0500

    Fixed CounterButton, added map icon

--- web/src/components/Events/EventItem.js
+++ web/src/components/Events/EventItem.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import "./EventItem.css";
 import CounterButton from "../CounterButton";
 import { useState } from 'react'
+import {FaMapMarkedAlt} from "react-icons/fa";
 
 const EventItem = (props) => {
 
@@ -11,15 +12,14 @@ const EventItem = (props) => {
   return (
     <Card className="expense-item">
     <EventDate sport={props.sport} />
-    <div className="location"> {props.location} </div>
+    <div className="location"> <FaMapMarkedAlt/> {props.location} </div>
     <div className="eventProperties">
       <div className="startAndDuration">
         <p> Started at {props.time}</p>
         <p> Playing for {Math.floor(props.duration/60)}h; {props.duration%60}m</p>
       </div> 
       <div className="numsHereComing"> 
-        <CounterButton text = {"Here"} hereToggled={hereToggled} setHereToggled={setHereToggled}/>
-        <CounterButton text = {"Coming"} hereToggled={hereToggled} setHereToggled={setHereToggled}/>
+        <CounterButton/>
       </div>
     </div>
         
